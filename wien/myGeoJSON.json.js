abc0a416edb9cc6ebc3d9e9c38d621a8cda1722e
SophiaPaul <sophia.paul@student.uibk.ac.at>
2017-05-03 17:33:53 +0200

    second point in GeoJSON and popup

--- wien/myGeoJSON.json.js
+++ wien/myGeoJSON.json.js
@@ -3,6 +3,32 @@
  */
 // variable is added to global window-object to be accessible from other locations
 window.my_geojson = {
-    "type": "Point", // key-value pair
-    "coordinates": [16.316, 48.227] // coordinates in latlng
+    "type": "FeatureCollection",
+    //every feature is an object and therefore written in {]
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point", // key-value pair
+                "coordinates": [16.316, 48.227] // coordinates in latlng
+            },
+            "properties": {
+                "NAME": "Friedhof Hernals",
+                "color": "black"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point", // key-value pair
+                "coordinates": [16.326, 48.227] // coordinates in latlng
+            },
+            "properties": {
+                "color": "green"
+            }
+        }
+
+    ]
+    //"type": "Point", // key-value pair
+    //"coordinates": [16.316, 48.227] // coordinates in latlng
 };
