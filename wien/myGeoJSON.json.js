5f72bbd5134ed8589e583324b17de9c31d8654c9
SophiaPaul <sophia.paul@student.uibk.ac.at>
2017-05-03 17:12:06 +0200

    added own GeoJSON
    a marker at Hernals cemetery

---
+++ wien/myGeoJSON.json.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Sophia on 03.05.2017.
+ */
+// variable is added to global window-object to be accessible from other locations
+window.my_geojson = {
+    "type": "Point", // key-value pair
+    "coordinates": [16.316, 48.227] // coordinates in latlng
+};
